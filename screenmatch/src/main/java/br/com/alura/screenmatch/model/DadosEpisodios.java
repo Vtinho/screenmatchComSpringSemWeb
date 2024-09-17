48ec1a95612163615d67bfe76c8d340e7ca4a55e
Vitor Manzotti <115799715+Vtinho@users.noreply.github.com>
2024-09-17 12:05:31 -0300

    Adicionando tradução da API do chat GPT

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosEpisodios.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosEpisodios.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-
-public record DadosEpisodeos(@JsonAlias("Title") String titulo,
+public record DadosEpisodios(@JsonAlias("Title") String titulo,
                              @JsonAlias("Episode") Integer numero,
                              @JsonAlias("imdbRating") String avaliacao,
                              @JsonAlias("Released") String dataLancamento) {
