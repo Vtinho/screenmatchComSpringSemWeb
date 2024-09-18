17e4ba6c3e44ed46b86124c3e24b28fa29f7a710
Vitor Manzotti <115799715+Vtinho@users.noreply.github.com>
2024-09-18 18:00:44 -0300

    Adicionando variaveis de ambiente e aplicando novas alterações

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,10 @@
 package br.com.alura.screenmatch;
 
 
+import br.com.alura.screenmatch.Repository.SerieRepository;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.principal.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,13 +15,16 @@ import java.util.List;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
+	@Autowired
+	private SerieRepository repositorio;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(repositorio);
 		principal.exibeMenu();
 	}
 }
